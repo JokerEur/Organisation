fcc02839983134df2860b3967d2076a5132464fa
Alina Burykina <alina4268@mail.ru>
2023-04-08 14:40:26 +0300

    db update

--- backend/database.py
+++ backend/database.py
@@ -1,45 +1,59 @@
 import sqlite3
 
-connection = sqlite3.connect('db/database.db')
-# connection.execute("PRAGMA foreign_keys = 1")
+# connection = sqlite3.connect('db/database.db')
+# cursor = connection.cursor()
 
-cursor = connection.cursor()
+def db_start():
+    global connection, cursor
 
-cursor.execute('''CREATE TABLE users(
-  id INTEGER PRIMARY KEY,
-  name TEXT NOT NULL,
-  type TEXT NOT NULL,
-  login TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL)''')
+    connection = sqlite3.connect('db/database.db')
+    # connection.execute("PRAGMA foreign_keys = 1")
 
-cursor.execute('''CREATE TABLE objects(
-  id INTEGER,
-  county TEXT NOT NULL,
-  district TEXT NOT NULL,
-  address TEXT NOT NULL,
-  object_type TEXT NOT NULL,
-  condition TEXT NOT NULL, 
-  square REAL NOT NULL,
-  owner TEXT NOT NULL,
-  actual_user TEXT NOT NULL,
-  media BLOB NOT NULL,
-  PRIMARY KEY(id, county , address)
-)''')
-               
-cursor.execute('''CREATE TABLE tasks(
-  id INTEGER PRIMARY KEY,
-  object_id INTEGER NOT NULL,
-  object_county TEXT NOT NULL,
-  object_address TEXT NOT NULL,
-  description TEXT NOT NULL,
-  status TEXT NOT NULL,
-  time_stamp NUMERIC NOT NULL,
-  deadline NUMERIC NOT NULL,
-  report TEXT NULL,
-  feedback TEXT NULL,
-  FOREIGN KEY (object_id ,object_county, object_address) REFERENCES objects(id , county, address)
-  
-)''')
+    cursor = connection.cursor()
 
-connection.commit()
-connection.close()
+    cursor.execute('''CREATE TABLE users(
+      id INTEGER PRIMARY KEY,
+      name TEXT NOT NULL,
+      type TEXT NOT NULL,
+      login TEXT UNIQUE NOT NULL,
+      password TEXT NOT NULL)''')
+
+    cursor.execute('''CREATE TABLE objects(
+      id INTEGER,
+      county TEXT NOT NULL,
+      district TEXT NOT NULL,
+      address TEXT NOT NULL,
+      object_type TEXT NOT NULL,
+      condition TEXT NOT NULL, 
+      square REAL NOT NULL,
+      owner TEXT NOT NULL,
+      actual_user TEXT NOT NULL,
+      media BLOB NOT NULL,
+      PRIMARY KEY(id, county , address)
+    )''')
+
+    cursor.execute('''CREATE TABLE tasks(
+      id INTEGER PRIMARY KEY,
+      object_id INTEGER NOT NULL,
+      object_county TEXT NOT NULL,
+      object_address TEXT NOT NULL,
+      description TEXT NOT NULL,
+      status TEXT NOT NULL,
+      time_stamp NUMERIC NOT NULL,
+      deadline NUMERIC NOT NULL,
+      report TEXT NULL,
+      feedback TEXT NULL,
+      FOREIGN KEY (object_id ,object_county, object_address) REFERENCES objects(id , county, address)
+      
+    )''')
+
+    connection.commit()
+    #connection.close()
+
+
+def create_user(id):
+    cursor.execute("INSERT OR IGNORE INTO users VALUES(?, ?, ?, ?, ?)",
+                   (id, '', '', '', ''))
+    connection.commit()
+    user = cursor.execute("SELECT 1 FROM users WHERE id == '{key}'".format(key=id)).fetchone()
+    return user
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
@@ -5,6 +7,12 @@ from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 import requests
 import json
+import database
+
+database.db_start()
+a = database.create_user(1)
+print(a)
+
 
 @csrf_exempt
 def index(request):
